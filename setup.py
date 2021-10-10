fe79044989d6eeda4a6b0b8065c9efafd6d82738
grtcdr <ba.tahaaziz@gmail.com>
2021-10-10 14:11:01 +0100

    bump to v1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="acfc",
-    version="1.1.1",
+    version="1.1.2",
     author="Aziz Ben Ali",
     author_email="ba.taahaziz@gmail.com",
     description="Alacritty Colors to Foot Colors (Converter)",
