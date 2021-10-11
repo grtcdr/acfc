946ad22948ab8fbaaa65a5ccdeab97608ca28f5a
grtcdr <ba.tahaaziz@gmail.com>
2021-10-11 20:59:06 +0100

    Better unit testing

--- acfc/__main__.py
+++ acfc/__main__.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import yaml
-import configparser
+import sys, os, yaml, configparser
 from acfc.colors import Colors
 
 
 def main():
-    if len(sys.argv) > 1:
-        yaml_file = sys.argv[1]
-
-        if os.path.isfile(yaml_file):
-            with open(yaml_file, 'r') as stream:
-                try:
-                    config = configparser.ConfigParser()
-                    colors = Colors(yaml.safe_load(stream)).as_foot(config)
-
-                    # Print to console
-                    for section in colors.sections():
-                        print('[' + section + ']')
-                        for item, value in config[section].items():
-                            print(item + '=' + value)
-
-                except yaml.YAMLError as exc:
-                    print(exc)
-        else:
-            print(yaml_file + ": Not a file.")
-            sys.exit(2)
-    else:
-        print("I need a YAML file to parse.")
+    if not len(sys.argv) > 1:
+        print('I need a YAML file to parse.')
         sys.exit(1)
+
+    yaml_file = sys.argv[1]
+    if not os.path.isfile(yaml_file):
+        print('"' + yaml_file + '": Not a file.')
+        sys.exit(2)
+    
+    extension = os.path.splitext(yaml_file)[1]
+    if extension != ".yml" and extension != ".yaml":
+        print('"' + yaml_file + '": Not a YAML file.')
+        sys.exit(3)
+
+    with open(yaml_file, 'r') as stream:
+        try:
+            config = configparser.ConfigParser()
+            colors = Colors(yaml.safe_load(stream)).as_foot(config)
+                
+            for section in colors.sections():
+                print('[' + section + ']')
+                for item, value in config[section].items():
+                    print(item + '=' + value)
+
+        except yaml.YAMLError as exc:
+            print(exc)
 
 
 if __name__ == '__main__':
--- tests/test_acfc.py
+++ tests/test_acfc.py
@@ -3,51 +3,50 @@ from acfc.colors import Colors
 import os
 
 
-def assert_bright(colors: Colors, black: str, blue: str, cyan: str, green: str, magenta: str, red: str, white: str, yellow: str):
-    assert colors.bright_black == black
-    assert colors.bright_blue == blue
-    assert colors.bright_cyan == cyan
-    assert colors.bright_green == green
-    assert colors.bright_magenta == magenta
-    assert colors.bright_red == red
-    assert colors.bright_white == white
-    assert colors.bright_yellow == yellow
-
 def test_version():
     assert __version__ == '2.0.4'
 
 
-def test_atom_yml():
+def test_themes_before_conversion():
+    """ Compares the parsed YML values for each theme in the 'themes' directory"""
     import yaml
 
-    file = open(os.path.dirname(__file__) + "/themes/Atom.yml", 'r')
-    with file as stream:
-        assert_bright(
-            Colors(yaml.safe_load(stream)),
-            '000000',
-            '96cbfe',
-            '85befd',
-            '94fa36',
-            'b9b6fc',
-            'fd5ff1',
-            'e0e0e0',
-            'f5ffa8'
-        )
+    themes = ["Atom", "Dracula"]
+    for i in themes:
+        theme_suffix = "/themes/" + i + ".yml"
+        file = open(os.path.dirname(__file__) + theme_suffix, 'r')
+        with file as stream:
+            colors = Colors(yaml.safe_load(stream))
+        
+            assert isinstance(int(colors.bright_black, 16), int) == True
+            assert isinstance(int(colors.bright_blue, 16), int) == True
+            assert isinstance(int(colors.bright_cyan, 16), int) == True
+            assert isinstance(int(colors.bright_green, 16), int) == True
+            assert isinstance(int(colors.bright_magenta, 16), int) == True
+            assert isinstance(int(colors.bright_red, 16), int) == True
+            assert isinstance(int(colors.bright_white, 16), int) == True
+            assert isinstance(int(colors.bright_yellow, 16), int) == True
 
 
-def test_dracula_yml():
+def test_themes_after_conversion():
+    """ Compares YML and INI values for each theme in the 'themes' directory"""
     import yaml
+    import configparser
 
-    file = open(os.path.dirname(__file__) + "/themes/Dracula.yml", 'r')
-    with file as stream:
-        assert_bright(
-            Colors(yaml.safe_load(stream)),
-            '555555',
-            'bd93f9',
-            '8be9fd',
-            '50fa7b',
-            'ff79c6',
-            'ff5555',
-            'ffffff',
-            'f1fa8c'
-        )
+    themes = ["Atom", "Dracula"]
+    for i in themes:
+        theme_suffix = "/themes/" + i + ".yml"
+        file = open(os.path.dirname(__file__) + theme_suffix, 'r')
+        with file as stream:
+            config = configparser.ConfigParser()
+            colors_yml = Colors(yaml.safe_load(stream))
+            colors_ini = colors_yml.as_foot(config)
+
+            assert colors_ini['colors']['bright0'] == colors_yml.bright_black
+            assert colors_ini['colors']['bright1'] == colors_yml.bright_red
+            assert colors_ini['colors']['bright2'] == colors_yml.bright_green
+            assert colors_ini['colors']['bright3'] == colors_yml.bright_yellow
+            assert colors_ini['colors']['bright4'] == colors_yml.bright_blue
+            assert colors_ini['colors']['bright5'] == colors_yml.bright_magenta
+            assert colors_ini['colors']['bright6'] == colors_yml.bright_cyan
+            assert colors_ini['colors']['bright7'] == colors_yml.bright_white
