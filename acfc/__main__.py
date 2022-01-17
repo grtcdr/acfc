6d8d39b6378ac0b6bf337caaaf0acf9c257146e0
grtcdr <ba.tahaaziz@gmail.com>
2022-01-17 18:16:46 +0100

    fix: loosen YAML requirements

--- acfc/__main__.py
+++ acfc/__main__.py
@@ -1,48 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import yaml
 import configparser
 from acfc import __version__
 from acfc.colors import Colors
 
 
+def not_blank(text: str):
+    return bool(text and not text.isspace())
+
+
 def print_ini(colors, config):
     for section in colors.sections():
         print('[' + section + ']')
         for item, value in config[section].items():
-            print(item + '=' + value)
+            if not_blank(value):
+                print(item + '=' + value)
 
 
 def main():
     config = configparser.ConfigParser()
 
-    if not len(sys.argv) > 1:
-        print('acfc, version ' + __version__)
-        print()
-        print('Paste in the Alacritty color configuration and press Ctrl+D as soon as you\'re finished:')
-        print()
-        colors = Colors(yaml.safe_load(sys.stdin.read())).as_foot(config)
-        sys.exit(print_ini(colors, config))
-
-    yaml_file = sys.argv[1]
-    if not os.path.isfile(yaml_file):
-        print('"' + yaml_file + '": Not a file.')
-        sys.exit(2)
-
-    extension = os.path.splitext(yaml_file)[1]
-    if extension != ".yml" and extension != ".yaml":
-        print('"' + yaml_file + '": Not a YAML file.')
-        sys.exit(3)
-
-    with open(yaml_file, 'r') as stream:
-        try:
-            colors = Colors(yaml.safe_load(stream)).as_foot(config)
-            print_ini(colors, config)
-
-        except yaml.YAMLError as exc:
-            print(exc)
+    print('acfc, version ' + __version__)
+    print()
+    print('Paste in the Alacritty color configuration and press Ctrl+D as soon as you\'re finished:')
+    print()
+    colors = Colors(yaml.safe_load(sys.stdin.read())).as_foot(config)
+    sys.exit(print_ini(colors, config))
 
 
 if __name__ == '__main__':
--- acfc/colors.py
+++ acfc/colors.py
@@ -1,112 +1,113 @@
 import configparser
-from acfc.helpers import unpound as up
+from acfc.helpers import unpound
 
+class Normal:
+    black = ''
+    blue = ''
+    cyan = ''
+    green = ''
+    magenta = ''
+    red = ''
+    yellow = ''
+    white = ''
+
+class Bright:
+    black = ''
+    blue = ''
+    cyan = ''
+    green = ''
+    magenta = ''
+    red = ''
+    yellow = ''
+    white = ''
+
+class Primary:
+    background = ''
+    foreground = ''
+
+class Cursor:
+    background = ''
+    foreground = ''
+
+class Selection:
+    background = ''
+    foreground = ''
 
 class Colors:
-    normal_black = ''
-    normal_blue = ''
-    normal_cyan = ''
-    normal_green = ''
-    normal_magenta = ''
-    normal_red = ''
-    normal_yellow = ''
-    normal_white = ''
+    normal = Normal
+    bright = Bright
+    primary = Primary
+    selection = Selection
+    cursor = Cursor
 
-    bright_black = ''
-    bright_blue = ''
-    bright_cyan = ''
-    bright_green = ''
-    bright_magenta = ''
-    bright_red = ''
-    bright_yellow = ''
-    bright_white = ''
+    def try_find(self, colors: dict, group: str, field: str):
+        try:
+            return unpound(colors['colors'][group][field])
+        except KeyError:
+            if group == 'bright':
+                return unpound(colors['colors']['normal'][field])
+            else:
+                return ''
 
-    primary_background = ''
-    primary_foreground = ''
-    selection_background = ''
-    selection_foreground = ''
-    cursor_background = ''
-    cursor_foreground = ''
-
-    def __init__(self, _dict: dict):
-        if not _dict:
+    def __init__(self, colors: dict):
+        if not colors:
             return None
         
-        self.normal_black = up(_dict['colors']['normal']['black'])
-        self.normal_blue = up(_dict['colors']['normal']['blue'])
-        self.normal_cyan = up(_dict['colors']['normal']['cyan'])
-        self.normal_green = up(_dict['colors']['normal']['green'])
-        self.normal_magenta = up(_dict['colors']['normal']['magenta'])
-        self.normal_red = up(_dict['colors']['normal']['red'])
-        self.normal_yellow = up(_dict['colors']['normal']['yellow'])
-        self.normal_white = up(_dict['colors']['normal']['white'])
+        self.normal.black = self.try_find(colors, 'normal', 'black')
+        self.normal.red = self.try_find(colors, 'normal', 'red')
+        self.normal.green = self.try_find(colors, 'normal', 'green')
+        self.normal.yellow = self.try_find(colors, 'normal', 'yellow')
+        self.normal.blue  = self.try_find(colors, 'normal', 'blue')
+        self.normal.magenta = self.try_find(colors, 'normal', 'magenta')
+        self.normal.cyan  = self.try_find(colors, 'normal', 'cyan')
+        self.normal.white = self.try_find(colors, 'normal', 'white')
 
-        try:
-            self.bright_black = up(_dict['colors']['bright']['black'])
-            self.bright_blue = up(_dict['colors']['bright']['blue'])
-            self.bright_cyan = up(_dict['colors']['bright']['cyan'])
-            self.bright_green = up(_dict['colors']['bright']['green'])
-            self.bright_magenta = up(_dict['colors']['bright']['magenta'])
-            self.bright_red = up(_dict['colors']['bright']['red'])
-            self.bright_yellow = up(_dict['colors']['bright']['yellow'])
-            self.bright_white = up(_dict['colors']['bright']['white'])
-        except KeyError:
-            print("Bright values were not specified, so I will default to normal values.")
-            self.bright_black = up(_dict['colors']['normal']['black'])
-            self.bright_blue = up(_dict['colors']['normal']['blue'])
-            self.bright_cyan = up(_dict['colors']['normal']['cyan'])
-            self.bright_green = up(_dict['colors']['normal']['green'])
-            self.bright_magenta = up(_dict['colors']['normal']['magenta'])
-            self.bright_red = up(_dict['colors']['normal']['red'])
-            self.bright_yellow = up(_dict['colors']['normal']['yellow'])
-            self.bright_white = up(_dict['colors']['normal']['white'])
+        self.bright.black = self.try_find(colors, 'bright', 'black')
+        self.bright.red = self.try_find(colors, 'bright', 'red')
+        self.bright.green = self.try_find(colors, 'bright', 'green')
+        self.bright.yellow = self.try_find(colors, 'bright', 'yellow')
+        self.bright.blue  = self.try_find(colors, 'bright', 'blue')
+        self.bright.magenta = self.try_find(colors, 'bright', 'magenta')
+        self.bright.cyan  = self.try_find(colors, 'bright', 'cyan')
+        self.bright.white = self.try_find(colors, 'bright', 'white')
 
-        try:
-            self.primary_background = up(
-                _dict['colors']['primary']['background'])
-            self.primary_foreground = up(
-                _dict['colors']['primary']['foreground'])
-            self.selection_background = up(
-                _dict['colors']['selection']['background'])
-            self.selection_foreground = up(
-                _dict['colors']['selection']['text'])
-            self.cursor_background = up(_dict['colors']['cursor']['cursor'])
-            self.cursor_foreground = up(_dict['colors']['cursor']['text'])
-        except KeyError:
-            raise Exception(
-                "This configuration file does not set some required keys.")
+        self.primary.background = self.try_find(colors, 'primary', 'background')
+        self.primary.foreground = self.try_find(colors, 'primary', 'foreground')
+
+        self.cursor.background = self.try_find(colors, 'cursor', 'background')
+        self.cursor.foreground = self.try_find(colors, 'cursor', 'foreground')
+
+        self.selection.background = self.try_find(colors, 'selection', 'background')
+        self.selection.foreground = self.try_find(colors, 'selection', 'foreground')
 
     def as_foot(self, config: configparser.ConfigParser):
         """
-        Populates a given ``config`` with proper color values
-
-        :param config: An object containing foot's configuration
-        :return: configparser.ConfigParser
+        Populates a given `config` with its appropriate values
         """
         config['cursor'] = {
-            'color': self.cursor_foreground + ' ' + self.cursor_background
+            'color': self.cursor.foreground + ' ' + self.cursor.background
         }
         config['colors'] = {
-            'foreground': self.primary_foreground,
-            'background': self.primary_background,
-            'regular0': self.normal_black,
-            'regular1': self.normal_red,
-            'regular2': self.normal_green,
-            'regular3': self.normal_yellow,
-            'regular4': self.normal_blue,
-            'regular5': self.normal_magenta,
-            'regular6': self.normal_cyan,
-            'regular7': self.normal_white,
-            'bright0': self.bright_black,
-            'bright1': self.bright_red,
-            'bright2': self.bright_green,
-            'bright3': self.bright_yellow,
-            'bright4': self.bright_blue,
-            'bright5': self.bright_magenta,
-            'bright6': self.bright_cyan,
-            'bright7': self.bright_white,
-            'selection-foreground': self.selection_foreground,
-            'selection-background': self.selection_background,
+            'foreground': self.primary.foreground,
+            'background': self.primary.background,
+            'regular0': self.normal.black,
+            'regular1': self.normal.red,
+            'regular2': self.normal.green,
+            'regular3': self.normal.yellow,
+            'regular4': self.normal.blue,
+            'regular5': self.normal.magenta,
+            'regular6': self.normal.cyan,
+            'regular7': self.normal.white,
+            'bright0': self.bright.black,
+            'bright1': self.bright.red,
+            'bright2': self.bright.green,
+            'bright3': self.bright.yellow,
+            'bright4': self.bright.blue,
+            'bright5': self.bright.magenta,
+            'bright6': self.bright.cyan,
+            'bright7': self.bright.white,
+            'selection-foreground': self.selection.foreground,
+            'selection-background': self.selection.background,
         }
 
         return config
